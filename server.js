a0c994401c245eb2d9a97a95c9f85db1c5324f2e
wittns <mugishawitness15@gmail.com>
2026-04-09 19:43:44 +0200

    first commit on io

---
+++ server.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import http from 'http'
+import {Server} from 'socket.io'
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+const httpServer = http.createServer(app)
+
+const io = new Server(httpServer,{
+    cors: {origin:"*"}
+})
+
+io.on("connection",(socket)=>{
+    console.log('connection made',socket.id)
+
+    socket.on('message',(data)=>{
+        console.log(`message received ${data}`)
+
+            io.emit('message',data)
+    })
+
+
+
+    socket.on('disconnect',()=>{
+        console.log('user disconnected:',socket.id)
+    })
+})
+
+httpServer.listen(PORT,()=>{
+    console.log(`server running on PORT : ${PORT}`)
+})
