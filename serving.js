f9a4ffb6de1393a1b2d99e7110b6575e838d43c7
wittns <mugishawitness15@gmail.com>
2026-04-10 19:49:10 +0200

    socket.io integration

--- serving.js
+++ serving.js
@@ -2,17 +2,24 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import http from 'http'
+import {Server} from 'socket.io'
 import {fileURLToPath} from 'node:url'
 import {dirname,join} from 'path'
 const PORT = process.env.PORTS || 4000
 const app  = express()
 
 const server = http.createServer(app)
-
+const io = new Server(server,{
+    cors: {origin : "*"}
+})
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.get('/',(req,res)=>{
     res.sendFile(join(__dirname,'server.html'))
+})
+
+io.on("connection",(socket)=>{
+    console.log(`new user connected ID: ${socket}`)
 })
 
 server.listen(PORT,()=>{
