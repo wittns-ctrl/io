cff45bd89e7b93c960298666dab801aa93d86a55
wittns <mugishawitness15@gmail.com>
2026-04-11 20:09:27 +0200

    enabling state recovery

--- serving.js
+++ serving.js
@@ -10,7 +10,7 @@ const app  = express()
 
 const server = http.createServer(app)
 const io = new Server(server,{
-    cors: {origin : "*"}
+    connectionStateRecovery: {}
 })
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
